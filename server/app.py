875048853b6da7b5ab76b1bfa327f6bd191a7dc0
tsomielvis@gmail.com <tsomielvis@gmail.com>
2024-10-22 11:22:08 +0300

    done

--- server/app.py
+++ server/app.py
@@ -3,53 +3,55 @@
 from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
-
 from models import db, Plant
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
 
 migrate = Migrate(app, db)
 db.init_app(app)
 
 api = Api(app)
 
-
 class Plants(Resource):
-
     def get(self):
         plants = [plant.to_dict() for plant in Plant.query.all()]
         return make_response(jsonify(plants), 200)
-
+    
     def post(self):
         data = request.get_json()
-
         new_plant = Plant(
             name=data['name'],
             image=data['image'],
-            price=data['price'],
+            price=data['price']
         )
-
         db.session.add(new_plant)
         db.session.commit()
-
-        return make_response(new_plant.to_dict(), 201)
-
+        return make_response(jsonify(new_plant.to_dict()), 201)
 
 api.add_resource(Plants, '/plants')
 
-
 class PlantByID(Resource):
-
     def get(self, id):
-        plant = Plant.query.filter_by(id=id).first().to_dict()
-        return make_response(jsonify(plant), 200)
-
+        plant = Plant.query.filter_by(id=id).first_or_404()
+        return make_response(jsonify(plant.to_dict()), 200)
+    
+    def patch(self, id):
+        data = request.get_json()
+        plant = Plant.query.filter_by(id=id).first_or_404()
+        for attr in data:
+            setattr(plant, attr, data[attr])
+        db.session.commit()
+        return make_response(jsonify(plant.to_dict()), 200)
+    
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first_or_404()
+        db.session.delete(plant)
+        db.session.commit()
+        return make_response('', 204)
 
 api.add_resource(PlantByID, '/plants/<int:id>')
-
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
